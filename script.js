76c3df61ac153b76ac9232ede01ee0432365c466
110101110 <maotui.z@outlook.com>
2024-01-12 16:04:15 +0100

    update

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenuButton = document.getElementById('mobile-menu');
+    const menuIcon = document.getElementById('menu-icon');
     const menuList = document.querySelector('.menu-list');
     const overlay = document.querySelector('.overlay');
+    const icon1 = document.querySelector('.icon1');
+    const icon2 = document.querySelector('.icon2');
 
-    mobileMenuButton.addEventListener('click', function () {
+    menuIcon.addEventListener('click', function () {
         menuList.classList.toggle('show');
         overlay.classList.toggle('show');
+
+        // Toggle between icon1 and icon2 when the menu is activated
+        if (menuList.classList.contains('show')) {
+            icon1.style.display = 'none';
+            icon2.style.display = 'block';
+        } else {
+            icon1.style.display = 'block';
+            icon2.style.display = 'none';
+        }
     });
 
     overlay.addEventListener('click', function () {
         menuList.classList.remove('show');
         overlay.classList.remove('show');
+
+        // Switch back to icon1 when the overlay is clicked to close the menu
+        icon1.style.display = 'block';
+        icon2.style.display = 'none';
     });
 });
